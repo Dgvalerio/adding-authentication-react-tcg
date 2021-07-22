3af1965198901c4124db4e8f5301221bd1a86e09
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-22 10:00:18 -0300

    Initial commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import StartingPageContent from '../components/StartingPage/StartingPageContent';
+
+const HomePage = () => <StartingPageContent />;
+
+export default HomePage;
